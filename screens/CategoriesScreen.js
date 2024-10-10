e20505a0d350dabd85f0a736f9141a63b5d1958e
Ilona Kauppila <123626072+ilokau@users.noreply.github.com>
2024-10-10 11:22:46 +0300

    Initial situation 2

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+function CategoriesScreen() {
+
+  function renderCategoryItem(itemData) {
+    return (
+      <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+    );
+  }
+  
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
